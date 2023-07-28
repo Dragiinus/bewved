1c4ad1182a3b415d8192f1939317e6e1c6057db2
Dragiinus <melvinmaute@gmail.com>
2023-07-28 19:36:53 +0200

    correction addlearner app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
-import React, { useState } from 'react';
+import React from 'react';
+import '@/App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Header from '@/components/Header';
+
 import AddLearner from '@/Learner/AddLearner';
 import ListLearner from '@/Learner/ListLearner';
+import Header from './components/Header';
 
 function App() {
-  const [selectedSession, setSelectedSession] = useState('');
-
   return (
+    
     <BrowserRouter>
-      <Header onSelectSession={setSelectedSession} />
+            <Header />
       <Routes>
-        {/* Pass the selectedSession prop to the ListLearner component */}
-        <Route path="/" element={<ListLearner selectedSession={selectedSession} />} />
-        <Route path="/learners" element={<ListLearner selectedSession={selectedSession} />} />
+        <Route path="/" element={<ListLearner />} />
+        <Route path="/learners" element={<ListLearner />} />
         <Route path="/learner" element={<AddLearner />} />
-        <Route path="/updateLearners/:id" element={<AddLearner />} />
+        {/* Pass the learner data as props to AddLearner component */}
+        <Route path="/updateLearner/:id" element={<AddLearner isUpdate />} />
       </Routes>
     </BrowserRouter>
   );
